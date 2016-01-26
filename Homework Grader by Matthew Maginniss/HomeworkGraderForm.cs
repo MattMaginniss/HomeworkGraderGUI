9a9b1bb1a58dbecc292e03334454bb2c08554b05
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-25 21:44:11 -0500

    Initial Commit

---
+++ Homework Grader by Matthew Maginniss/HomeworkGraderForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_Grader_by_Matthew_Maginniss
+{
+    public partial class HomeworkGraderForm : Form
+    {
+        public HomeworkGraderForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

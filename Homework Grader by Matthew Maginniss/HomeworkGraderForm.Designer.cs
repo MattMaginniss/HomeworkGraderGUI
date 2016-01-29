890b5bea9dc7e6c5a3cf447236a066df5423eb7e
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-28 21:17:52 -0500

    Set up the Form:  Added tab controls with the grader form.  Added code to initialize values of the form control.  Added Output box and button for copying.

--- Homework Grader by Matthew Maginniss/HomeworkGraderForm.Designer.cs
+++ Homework Grader by Matthew Maginniss/HomeworkGraderForm.Designer.cs
@@ -28,20 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageFunctionality = new System.Windows.Forms.TabPage();
+            this.tabPageImplementation = new System.Windows.Forms.TabPage();
+            this.tabPageDocumentation = new System.Windows.Forms.TabPage();
+            this.controlFunctionality = new GraderFormControl.HomeworkFormControl();
+            this.controlImplementation = new GraderFormControl.HomeworkFormControl();
+            this.controlDocumentation = new GraderFormControl.HomeworkFormControl();
+            this.tabControl.SuspendLayout();
+            this.tabPageFunctionality.SuspendLayout();
+            this.tabPageImplementation.SuspendLayout();
+            this.tabPageDocumentation.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(17, 430);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(544, 279);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(252, 725);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPageFunctionality);
+            this.tabControl.Controls.Add(this.tabPageImplementation);
+            this.tabControl.Controls.Add(this.tabPageDocumentation);
+            this.tabControl.Location = new System.Drawing.Point(13, 26);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(552, 362);
+            this.tabControl.TabIndex = 3;
+            // 
+            // tabPageFunctionality
+            // 
+            this.tabPageFunctionality.Controls.Add(this.controlFunctionality);
+            this.tabPageFunctionality.Location = new System.Drawing.Point(4, 22);
+            this.tabPageFunctionality.Name = "tabPageFunctionality";
+            this.tabPageFunctionality.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageFunctionality.Size = new System.Drawing.Size(544, 336);
+            this.tabPageFunctionality.TabIndex = 0;
+            this.tabPageFunctionality.Text = "Functionality";
+            this.tabPageFunctionality.UseVisualStyleBackColor = true;
+            // 
+            // tabPageImplementation
+            // 
+            this.tabPageImplementation.Controls.Add(this.controlImplementation);
+            this.tabPageImplementation.Location = new System.Drawing.Point(4, 22);
+            this.tabPageImplementation.Name = "tabPageImplementation";
+            this.tabPageImplementation.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageImplementation.Size = new System.Drawing.Size(544, 336);
+            this.tabPageImplementation.TabIndex = 1;
+            this.tabPageImplementation.Text = "Implementation";
+            this.tabPageImplementation.UseVisualStyleBackColor = true;
+            // 
+            // tabPageDocumentation
+            // 
+            this.tabPageDocumentation.Controls.Add(this.controlDocumentation);
+            this.tabPageDocumentation.Location = new System.Drawing.Point(4, 22);
+            this.tabPageDocumentation.Name = "tabPageDocumentation";
+            this.tabPageDocumentation.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDocumentation.Size = new System.Drawing.Size(544, 336);
+            this.tabPageDocumentation.TabIndex = 2;
+            this.tabPageDocumentation.Text = "Documentation";
+            this.tabPageDocumentation.UseVisualStyleBackColor = true;
+            // 
+            // controlFunctionality
+            // 
+            this.controlFunctionality.Location = new System.Drawing.Point(-3, -1);
+            this.controlFunctionality.Name = "controlFunctionality";
+            this.controlFunctionality.Size = new System.Drawing.Size(551, 338);
+            this.controlFunctionality.TabIndex = 0;
+            // 
+            // controlImplementation
+            // 
+            this.controlImplementation.Location = new System.Drawing.Point(-3, -1);
+            this.controlImplementation.Name = "controlImplementation";
+            this.controlImplementation.Size = new System.Drawing.Size(551, 338);
+            this.controlImplementation.TabIndex = 0;
+            // 
+            // controlDocumentation
+            // 
+            this.controlDocumentation.Location = new System.Drawing.Point(-3, -1);
+            this.controlDocumentation.Name = "controlDocumentation";
+            this.controlDocumentation.Size = new System.Drawing.Size(551, 338);
+            this.controlDocumentation.TabIndex = 4;
             // 
             // HomeworkGraderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(579, 775);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtOutput);
             this.Name = "HomeworkGraderForm";
             this.Text = "Homework Grader by Matthew Maginniss";
+            this.tabControl.ResumeLayout(false);
+            this.tabPageFunctionality.ResumeLayout(false);
+            this.tabPageImplementation.ResumeLayout(false);
+            this.tabPageDocumentation.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageFunctionality;
+        private System.Windows.Forms.TabPage tabPageImplementation;
+        private GraderFormControl.HomeworkFormControl homeworkFormControl1;
+        private System.Windows.Forms.TabPage tabPageDocumentation;
+        private GraderFormControl.HomeworkFormControl controlFunctionality;
+        private GraderFormControl.HomeworkFormControl controlImplementation;
+        private GraderFormControl.HomeworkFormControl controlDocumentation;
     }
 }
 
--- Homework Grader by Matthew Maginniss/HomeworkGraderForm.cs
+++ Homework Grader by Matthew Maginniss/HomeworkGraderForm.cs
@@ -1,20 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Homework_Grader_by_Matthew_Maginniss
 {
     public partial class HomeworkGraderForm : Form
     {
+        #region Constructors
+
         public HomeworkGraderForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.initializeFunctionalityControl();
+            this.initializeImplementationControl();
+            this.initializeDocumentationControl();
+        }
+
+        #endregion
+
+        private void initializeDocumentationControl()
+        {
+            this.controlDocumentation.SetPointValue(3, 2, 1, 0);
+            this.controlDocumentation.AddComment("Missing several specification headers for public interface of class.");
+        }
+
+        private void initializeImplementationControl()
+        {
+            this.controlImplementation.SetPointValue(6, 4, 2, 0);
+            this.controlImplementation.AddComment("Magic numbers are used within the code.");
+            this.controlImplementation.AddComment("Overall class design not following best practices in class design.");
+            this.controlImplementation.AddComment("Duplicated code needs to be refactored.");
+        }
+
+        private void initializeFunctionalityControl()
+        {
+            this.controlFunctionality.SetPointValue(11, 8, 3, 0);
+            this.controlFunctionality.AddComment("Good Job!");
+            this.controlFunctionality.AddComment("No issues were discovered");
         }
     }
 }
